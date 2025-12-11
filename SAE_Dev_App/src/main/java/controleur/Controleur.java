4a553a3289d4b53622a86209edd06bd31e471a59
Milka <mathis.graine51@gmail.com>
2025-12-11 15:14:46 +0100

    iteration 1

--- SAE_Dev_App/src/main/java/controleur/Controleur.java
+++ SAE_Dev_App/src/main/java/controleur/Controleur.java
@@ -1,4 +1,16 @@
 package controleur;
 
+import modele.DataManager;
+import modele.Tache;
+
 public class Controleur {
+
+    // Méthode appelée quand l'utilisateur veut créer une tâche
+    public void creerTache(String titre) {
+        System.out.println("[Controleur] Demande de création : " + titre);
+        Tache t = new Tache(titre);
+
+        // Le contrôleur modifie le Modèle
+        DataManager.getInstance().ajouterTache(t);
+    }
 }
