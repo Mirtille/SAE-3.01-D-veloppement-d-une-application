1a8262b9d575adc4b8614d2b2c2aca5e1dca86f4
Milka <mathis.graine51@gmail.com>
2026-01-07 15:26:32 +0100

    vueMenu

--- SAE_Dev_App/src/main/java/vue/VueMenu.java
+++ SAE_Dev_App/src/main/java/vue/VueMenu.java
@@ -3,11 +3,8 @@ package vue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import modele.Projet;
 import modele.SingletonTache;
 
